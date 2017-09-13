b79b099e0dd0bf1e284abcdaa65e6957fa7684c8
maja55 <majapodrug2@gmail.com>
2017-09-13 23:04:04 +0200

    ajax call for email request

--- email-request.php
+++ email-request.php
@@ -1,5 +1,4 @@
 <?php
-if(isset($_POST))
 {
     $visitor_email = $_POST['email'];
 
@@ -17,16 +16,15 @@ if(isset($_POST))
 
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
-    if(!isset($visitor_email)) {
-        echo '<script type="text/javascript"> alert ("Email address is required"); window.history.back();</script>';
-    }
-    else if (!preg_match($email_exp,$visitor_email)) {
-        echo '<script type="text/javascript"> alert ("The email address you entered does not appear to be valid. Please try again."); window.history.back(); </script>';
-    }
-    else {
-        mail($to,$email_subject,$email_body,$headers);
-
-        echo '<script type="text/javascript"> alert ("Thank you for your interest. We have received your email and will get back to you as soon as possible."); window.history.back(); </script>';
+    if((!isset($visitor_email)) || (!preg_match($email_exp,$visitor_email))) {
+        echo "Please enter a valid email address.";
+    } else {
+        if (mail($to,$email_subject,$email_body,$headers)) {
+            echo "Thank you for your interest. We have received your email and will get back to you as soon as possible.";
+        } else {
+            echo "Sorry, something went wrong. Please contact us directly at grow@monolith.co";
+        }
     }
 }
+
 ?>
